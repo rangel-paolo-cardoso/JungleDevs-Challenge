ff312576e1486f5f528e8a40a4e4d6888297c66b
Rangel Paolo <paolorangel1996@outlook.com>
2021-09-07 22:15:09 -0300

    3 first section in mobile done

---
+++ src/components/DescriptionSection/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import imageSection1 from '../../assets/description-section/ImageSection1.png';
+
+import './styles.css';
+
+const DescriptionSection = () => {
+  return (
+    <section className="description-section">
+      <img src={imageSection1} alt="Example of nanny location and costs." />
+      <h3>Share your home, nanny and costs</h3>
+      <p>
+        You have a fantastic home, a fantastic nanny and wouldn’t cutting
+        your costs in half be, well, fantastic?!
+        If only it was easy to connect with other parents to share your costs?
+        Well now it is, with Hapu. <span>Hapu means tribe</span> and
+        it’s our foundational
+        3 tribal principles that empowers you to create and manage your own tribe.
+        A tribe that together has the power to create new affordable solutions in
+        childcare that work for you and your community.
+      </p>
+      <a href="/#">Ready to get started?</a>
+      <div className="gray-bar"/>
+    </section>
+  );
+};
+
+export default DescriptionSection;
---
+++ src/components/ExampleSection/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import profileImage from '../../assets/example-section/ProfileImage.png';
+
+import './styles.css';
+
+const ExamploSection = () => {
+  return (
+    <section className="example-section">
+      <img src={profileImage} alt="A woman's profile." />
+      <p>Sarah’s day care available now in North Sydney</p>
+      <p>Wednesday, Thursday, Friday - 7:30 - 5:30</p>
+    </section>
+  );
+};
+
+export default ExamploSection;
