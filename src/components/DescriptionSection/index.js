0b09d1b7f646205782a3736eead9060a9d1913a1
Rangel Paolo <paolorangel1996@outlook.com>
2021-09-08 05:46:11 -0300

    Contact section done for pcs

--- src/components/DescriptionSection/index.js
+++ src/components/DescriptionSection/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import GrayBar from '../GrayBar';
 
-import imageSection1 from '../../assets/description-section/ImageSection1.png';
+import imageSection1Large from '../../assets/description-section/ImageSection1Large.png';
 
 import './styles.css';
 
 const DescriptionSection = () => {
   return (
     <section className="description-section">
-      <img src={imageSection1} alt="Example of nanny location and costs." />
-      <h3>Share your home, nanny and costs</h3>
-      <p>
-        You have a fantastic home, a fantastic nanny and wouldn’t cutting
-        your costs in half be, well, fantastic?!
-        If only it was easy to connect with other parents to share your costs?
-        Well now it is, with Hapu. <span>Hapu means tribe</span> and
-        it’s our foundational
-        3 tribal principles that empowers you to create and manage your own tribe.
-        A tribe that together has the power to create new affordable solutions in
-        childcare that work for you and your community.
-      </p>
-      <a href="/#">Ready to get started?</a>
-      <GrayBar />
+      <div>
+        <h3>Share your home, nanny and costs</h3>
+        <p>
+          You have a fantastic home, a fantastic nanny and wouldn’t cutting
+          your costs in half be, well, fantastic?!
+          If only it was easy to connect with other parents to share your costs?
+          Well now it is, with Hapu. <span>Hapu means tribe</span> and
+          it’s our foundational
+          3 tribal principles that empowers you to create and manage your own tribe.
+          A tribe that together has the power to create new affordable solutions in
+          childcare that work for you and your community.
+        </p>
+        <a href="/#">Ready to get started?</a>
+      </div>
+      <img src={imageSection1Large} alt="Example of nanny location and costs." />
     </section>
   );
 };
--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ContactSection from '../ContactSection';
 import DescriptionSection from '../DescriptionSection';
 
+import GrayBar from '../GrayBar';
+
 import './styles.css';
 
 const MainSection = () => {
   return (
     <main className="main-section">
       <DescriptionSection />
+      <GrayBar />
       <ContactSection />
     </main>
   );
